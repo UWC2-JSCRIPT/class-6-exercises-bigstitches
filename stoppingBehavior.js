1b3fda06c84dec24b98bbbed2bad46c549d6f136
Eileen B <eileen.e.beck@gmail.com>
2022-12-03 07:37:59 -0800

    complete

--- stoppingBehavior.js
+++ stoppingBehavior.js
@@ -5,6 +5,14 @@ document.getElementById('cat').addEventListener('click', () => {
 
 // When clicked, "More info" link should alert "Here's some info"
 // instead of going to a new webpage
+document.getElementById('more-info').addEventListener('click', function (event) {
+  alert("Here's some info");
+  event.preventDefault();
+});
 
 // When the bark button is clicked, should alert "Bow wow!"
 // Should *not* alert "meow"
+document.getElementById('dog').addEventListener('click', function(event) {
+  alert('bark');
+  event.stopPropagation();
+});
