1f1dc2b4aa62ad8b416d971aa1eb84bcbdb9141b
Eileen B <eileen.e.beck@gmail.com>
2022-12-03 14:52:24 -0800

    dom

--- jQueryDomCrud.js
+++ jQueryDomCrud.js
@@ -1,3 +1,7 @@
+/*
+Add the jQuery script tag to your document.
+*/
+
 // Create a new <a> element containing the text "Buy Now!"
 // with an id of "cta" after the last <p>
 
@@ -13,3 +17,41 @@
 // Create a listener on the "Buy Now!" link that responds to a click event.
 // When clicked, the the "Buy Now!" link should be removed
 // and replaced with text that says "Added to cart"
+
+//1. Wrap your JS code in the jQuery document.ready method
+$(document).ready( function () {
+    
+    // 2. Access (read) the data-color attribute of the <img>, log to the console.
+    console.log($('img').attr('data-color'));
+    console.log($('img').data('color'));
+    
+    
+    // 3. Update the third <li> item ("Turbocharged"), set the class name to "highlight"
+    const $listItems = $('li');
+    $listItems[2].className = 'highlight';
+    console.log($listItems[2].className);
+
+    
+    // 4. Create a new <a> element containing the text "Buy Now!" with an id of "cta" after the last <p>
+    $a = $('<a>');
+    $a.text('Buy Now!');
+    $a.attr('id', 'cta');
+    
+    //console.log($a);
+    //$('h1').prepend($a);
+    //$('p').append($a); // last element within
+    $('p').after($a);
+
+    //5. Remove (delete) the last paragraph ("Available for purchase now...")
+    $('p').remove();
+
+    // 6. Create a listener on the "Buy Now!" link that responds to a click event.  
+    // When clicked, the the "Buy Now!" link should be removed and replaced with text that says "Added to cart"
+    $( "#cta" ).click(function() {
+        $a.attr('id', 'atc');
+        $a.text('Added to Cart');      
+    });
+    
+
+})
+
