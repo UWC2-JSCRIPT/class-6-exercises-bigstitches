3282537c33c2d131bdd68e3964218d9237f216ea
Eileen B <eileen.e.beck@gmail.com>
2022-12-03 16:56:33 -0800

    ugh, finally done

--- jQueryToDoList.js
+++ jQueryToDoList.js
@@ -1,19 +1,49 @@
-/**
- * Toggles "done" class on <li> element
- */
+$(document).ready( function () {
+  /**
+  * Adds new list item to <ul> and a listener event to the new <a> of class 'delete'
+  */
+  const addListItem = function(event) {
+    event.preventDefault();
+    const $text = $('input').val();
 
-/**
- * Delete element when delete link clicked
- */
+    // create all the elements, list/span/a 
+    const $list = $('<li>');
+    const $span = $('<span>');
+    $span.text($text);
+    const $a = $('<a>');
+    $a.attr('class', 'delete').text('Delete');
 
-/**
- * Adds new list item to <ul>
- */
-const addListItem = function(e) {
-  e.preventDefault();
-  const text = $('input').val();
+    // bundle the elements for style
+    $list.append($span);
+    $list.append($a);
 
-  // rest here...
-};
+    // add a listener to the new 'delete' class <a>
+    $a.click( function () {$(this).parent().remove();})
+    // add a listener to the new <li>
+    $list.on('click', function(event) { $(this).toggleClass('done');})
 
-// add listener for add
+    // put on the ul of class today-list
+    $(".today-list").append($list);
+
+  };
+
+  /**
+  * Delete element when delete link clicked
+  */
+  $(".delete").click( function () {
+    //console.log($(this));
+    //console.log($(this).parent());
+    $(this).parent().remove();
+  })
+
+  // event listener to add items
+  $(".add-item").click(addListItem);
+
+  /**
+  * Toggles "done" class on <li> element
+  */
+  $('li').on('click', function(event) {
+    $(this).toggleClass('done');
+  })
+});
+
